d3fc65d8f4ca8a51946e36c8f402354469a81675
Jasonspd <jasoncluu@googlemail.com>
2015-06-27 18:30:57 +0100

    test

--- api/config.js
+++ api/config.js
@@ -1,15 +1,18 @@
-module.exports = {
-	google : {
-				password 	: process.env.GOOGLEPASSWORD		|| require('./creds.json').google.password,
-				clientId	: process.env.GOOGLECLIENTID 		|| require('./creds.json').google.clientId,
-				clientSecret	: process.env.GOOGLECLIENTSECRET	|| require('./creds.json').google.clientSecret,
-	},
-	facebook : {
-				password 	: process.env.FACEBOOKPASSWORD		|| require('./creds.json').facebook.password,
-				clientId	: process.env.FACEBOOKCLIENTID 		|| require('./creds.json').facebook.clientId,
-				clientSecret	: process.env.FACEBOOKCLIENTSECRET 	|| require('./creds.json').facebook.clientSecret,
-	},
-	cookie : {
-				password: process.env.COOKIEPASSWORD || require('./creds.json').cookiePassword
-	}
-};
+// module.exports = {
+	
+// 	google : {
+// 				password 		: process.env.GOOGLEPASSWORD			|| require('./creds.json').google.password,
+// 				clientId		: process.env.GOOGLECLIENTID 			|| require('./creds.json').google.clientId,
+// 				clientSecret	: process.env.GOOGLECLIENTSECRET		|| require('./creds.json').google.clientSecret,
+// 	},
+
+// 	facebook : {
+// 				password 		: process.env.FACEBOOKPASSWORD			|| require('./creds.json').facebook.password,
+// 				clientId		: process.env.FACEBOOKCLIENTID 			|| require('./creds.json').facebook.clientId,
+// 				clientSecret	: process.env.FACEBOOKCLIENTSECRET 		|| require('./creds.json').facebook.clientSecret,
+// 	},
+
+// 	cookie : {
+// 				password: process.env.COOKIEPASSWORD 				|| require('./creds.json').cookiePassword
+// 	}
+// };
