31cc582faa33714dd0478ce11897607d4c7a59d9
Jasonspd <jasoncluu@googlemail.com>
2015-07-13 01:19:21 +0100

    one job function

--- api/models/jobs.js
+++ api/models/jobs.js
@@ -1,8 +1,10 @@
 var mongoose = require("mongoose");
 var Job = require("./schema").Job;
+var User = require("./schema").User;
 
 function newjob(data, callback) {
 	console.log("new job form: ", data);
+	
 	var newJob = new Job(data);
 	Job.create(newJob, function (err, data) {
 		if (err) {
@@ -13,6 +15,20 @@ function newjob(data, callback) {
 		}
 	});
 }
+
+// function newjob(data, callback) {
+// 	console.log("new job form: ", data);
+
+// 	var currentUser = "jasoncluu@gmail.com"; //Adding mock user because no session
+
+// 	var query = {email: currentUser};
+// 	var update = {$push: {"jobs": data}};
+// 	var options = {new: true};
+
+// 	User.findOneAndUpdate(query, update, options, function (err, user) {
+// 		console.log("Found and updated a user with a new job");
+// 	});
+// }
 
 module.exports = {
 	newjob : newjob
