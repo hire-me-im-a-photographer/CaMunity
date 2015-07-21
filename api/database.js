29f59b1ccc788f69752d540fdc6ffebbbf5935d3
Jasonspd <jasoncluu@googlemail.com>
2015-07-21 10:38:38 +0100

    remote db again

--- api/database.js
+++ api/database.js
@@ -11,10 +11,10 @@ var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
  
 //REMOTE DB
-// mongoose.connect(Config.database.dburl, options);
+mongoose.connect(Config.database.dburl, options);
 
 //LOCAL DB
-mongoose.connect('mongodb://127.0.0.1:27017/camunity');
+// mongoose.connect('mongodb://127.0.0.1:27017/camunity');
 
 var db = mongoose.connection;
 
