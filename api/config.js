a67a14d7fa79ff6a65a04d44f4ae31d2f338aed7
Jasonspd <jasoncluu@googlemail.com>
2015-07-21 17:16:54 +0100

    s3 creds

--- api/config.js
+++ api/config.js
@@ -1,22 +1,33 @@
+var creds = require("./creds.json");
+
 module.exports = {
 
 	database: {
-		dburl: process.env.DBURL || require('./creds.json').database.dburl
+		dburl: process.env.DBURL 						|| creds.database.dburl
+	},
+
+	s3: {
+		key: process.env.S3KEY 							|| creds.s3.key,
+		secret: process.env.S3SECRET 					|| creds.s3.secret,
+		bucket: process.env.S3BUCKET 					|| creds.s3.bucket,
+		acl: process.env.S3ACL 							|| creds.s3.acl,
+		region: process.env.S3REGION 					|| creds.s3.region,
 	},
 	
 	google : {
-				password 		: process.env.GOOGLEPASSWORD			|| require('./creds.json').google.password,
-				clientId		: process.env.GOOGLECLIENTID 			|| require('./creds.json').google.clientId,
-				clientSecret	: process.env.GOOGLECLIENTSECRET		|| require('./creds.json').google.clientSecret,
+		password 		: process.env.GPASSWORD			|| creds.google.password,
+		clientId		: process.env.GCLIENTID 		|| creds.google.clientId,
+		clientSecret	: process.env.GSECRET			|| creds.google.clientSecret,
 	},
 
 	facebook : {
-				password 		: process.env.FACEBOOKPASSWORD			|| require('./creds.json').facebook.password,
-				clientId		: process.env.FACEBOOKCLIENTID 			|| require('./creds.json').facebook.clientId,
-				clientSecret	: process.env.FACEBOOKCLIENTSECRET 		|| require('./creds.json').facebook.clientSecret,
+		password 		: process.env.FBPASSWORD		|| creds.facebook.password,
+		clientId		: process.env.FBCLIENTID 		|| creds.facebook.clientId,
+		clientSecret	: process.env.FBSECRET 			|| creds.facebook.clientSecret,
 	},
 
 	cookie : {
-				password: process.env.COOKIEPASSWORD 				|| require('./creds.json').cookiePassword
+		password 		: process.env.COOKIEPASS		|| creds.cookie.password,
+		cookie 			: process.env.COOKIE 			|| creds.cookie.cookie
 	}
 };
