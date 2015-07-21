f04e3c98c8c438c956bbbd2e4fe548cb096a2bf3
Jasonspd <jasoncluu@googlemail.com>
2015-07-21 10:39:41 +0100

    users models console logs

--- api/models/users.js
+++ api/models/users.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose");
 var User = require("./schema").User;
 
 function addUser (data, callback) {
-	console.log("new user: ", data);
 	var newUser = new User(data);
 
 	User.create(newUser, function (err, data) {
@@ -18,7 +17,6 @@ function addUser (data, callback) {
 }
 
 function getUser (data, callback) {
-	console.log("getting user: ", data);
 	User.findOne( {id: data}, function(err, data){
 		if (err) {
 			return callback(err);
