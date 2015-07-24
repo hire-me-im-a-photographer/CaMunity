652d40998882013466ceb0438e7ea2a471840a40
Jasonspd <jasoncluu@googlemail.com>
2015-07-24 23:56:25 +0100

    delete comment

--- api/database.js
+++ api/database.js
@@ -1,12 +1,6 @@
 var Config = require("./config");
 var mongoose = require('mongoose');
 
-/* 
- * Mongoose by default sets the auto_reconnect option to true.
- * We recommend setting socket options at both the server and replica set level.
- * We recommend a 30 second connection timeout because it allows for 
- * plenty of time in most operating environments.
- */
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
  
