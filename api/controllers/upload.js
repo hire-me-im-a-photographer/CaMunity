6b99c7f8c7241b436d4d1315b2481f02f64f4ee6
Jasonspd <jasoncluu@googlemail.com>
2015-07-28 17:48:01 +0100

    watermark upload logic

--- api/controllers/upload.js
+++ api/controllers/upload.js
@@ -1,6 +1,6 @@
-var Config 	= require("../config");
-var Aws 	= require("aws-sdk");
-var Photos 	= require("../models/photos");
+var Config = require("../config");
+var Photos = require("../models/photos");
+var Aws = require("aws-sdk");
 
 module.exports = {
 
@@ -22,6 +22,7 @@ module.exports = {
 			var id = request.auth.credentials.id;
 			var query = request.query;
 			var randomKey = Math.floor((Math.random() * 1000) + 1);
+			var filepath = id + "/watermark/" + randomKey + "_" + query.file_name;
 
 			//S3 setup
 			Aws.config.update({
@@ -32,7 +33,7 @@ module.exports = {
 			var s3 = new Aws.S3();
 			var s3_params = {
 			    Bucket: Config.s3.bucket,
-			    Key: id + "/" + randomKey + "_" + query.file_name,
+			    Key: filepath,
 			    Expires: 60,
 			    ContentType: query.file_type,
 			    ACL: Config.s3.acl
@@ -48,8 +49,51 @@ module.exports = {
 			            signed_request: data,
 			            url: 
 			            "https://" + Config.s3.bucket + 
-			            ".s3.amazonaws.com/" + id + "/" + randomKey + 
-			            "_" + query.file_name,
+			            ".s3.amazonaws.com/" + filepath,
+			        };
+			        reply(JSON.stringify(return_data));
+			    }
+			});
+		}
+	},
+
+	watermark: {
+		auth: {
+			mode: "optional"
+		},
+		handler: function (request, reply) {
+
+			var id = request.auth.credentials.id;
+			var query = request.query;
+			var randomKey = Math.floor((Math.random() * 1000) + 1);
+			var filepath = id + "/watermark/" + randomKey + "_" + query.file_name;
+
+			//S3 setup
+			Aws.config.update({
+				accessKeyId: Config.s3.key, 
+				secretAccessKey: Config.s3.secret
+			});
+
+			var s3 = new Aws.S3();
+			var s3_params = {
+			    Bucket: Config.s3.bucket,
+			    Key: filepath,
+			    Expires: 60,
+			    ContentType: query.file_type,
+			    ACL: Config.s3.acl
+			};
+			// request.query.file_type (type of file you want to upload)
+
+			s3.getSignedUrl("putObject", s3_params, function (err, data){
+			    if (err) {
+			        console.log(err);
+			    }
+			    else {
+			        var return_data = {
+			            signed_request: data,
+			            url: 
+			            "https://" + Config.s3.bucket + 
+			            ".s3.amazonaws.com/" + filepath,
 			        };
 			        reply(JSON.stringify(return_data));
 			    }
