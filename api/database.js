edc0cb30f9a66e714bbaffd7ab19ac26edc407f5
Jasonspd <jasoncluu@googlemail.com>
2015-08-02 23:21:29 +0100

    db

--- api/database.js
+++ api/database.js
@@ -3,17 +3,23 @@ var Mongoose = require('mongoose');
 
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
- 
+
+//Check with heroku
+var dbtype; 
+
 //REMOTE DB
 Mongoose.connect(Config.database.dburl, options);
+dbtype = "remote DB";
 
 //LOCAL DB
 // Mongoose.connect('mongodb://127.0.0.1:27017/camunity');
+// dbtype = "local DB";
+
 
 var db = Mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function (callback) {
-	console.log("database connection successful");
+	console.log("Connected to " + dbtype);
 });
